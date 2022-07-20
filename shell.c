f591f614c0f5dd5392d5ffc01e2faeb77fe301c8
actatum88 <4946@holbertonschool.com>
2022-07-20 14:27:29 -0700

    print $

--- shell.c
+++ shell.c
@@ -1,8 +1,30 @@
-#include "main.h"
+#include "simpleshell.h"
 
 /**
- * betty - make a beautiful code for Betty
+ * av - NULL terminated array of strings
+ * ac - number of items in av
  */
 
+int main (void)
 {
+	char *buffer = NULL;
+	char **command;
+	size_t bufsize = 0;
+
+	while(1)
+	{
+		printf("($) ");
+
+		getline(&buffer, &bufsize, stdin);
+
+		if (strcmp(buffer, "exit\n") == 0)
+			break;
+		else
+		{
+			printf("Got to the command section!");
+			/*command = make_av(buffer);
+			if (execute(command) == -1)
+			break; */
+		}
+	}
 }
--- simpleshell.h
+++ simpleshell.h
@@ -1,9 +1,11 @@
-#ifndef KSHELL_H
-#define KSHELL_H
+#ifndef SIMPLESHELL_H
+#define SIMPLESHELL_H
 
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
 #include <string.h>
+
+extern int execle (const char *__path, const char *__arg, ...);
 
 #endif
