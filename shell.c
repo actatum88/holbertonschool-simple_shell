1430f5e64ab56d24b3eb8802ed5395b41216427f
actatum88 <4946@holbertonschool.com>
2022-07-19 22:13:38 -0700

    making files

---
+++ shell.c
@@ -0,0 +1,8 @@
+#include "main.h"
+
+/**
+ * betty - make a beautiful code for Betty
+ */
+
+{
+}
---
+++ simpleshell.h
@@ -0,0 +1,9 @@
+#ifndef KSHELL_H
+#define KSHELL_H
+
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+
+#endif
