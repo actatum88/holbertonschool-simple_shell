0ef4ccf2b96c1b6aa85ec2e66cffcc38c8af518f
root <4908@holbertonschool.com>
2022-07-28 08:18:32 -0700

    clean up code, clean up directory, deleted unused code

--- builtins.c
+++ builtins.c
@@ -53,22 +53,6 @@ int _printenv(void)
 	return (1);
 }
 
-char *_getenv(const char *name)
-{
-	unsigned int i = 0;
-	int check;
-	int len = strlen(name);
-
-	while (environ[i])
-	{
-		check = strcmp(environ[i], name);
-		if (check == 0)
-			return (environ[i] + len + 1);
-		i++;
-	}
-	return (NULL);
-}
-
 int _myexit(void)
 {
 	return (-1);
--- shell.c
+++ shell.c
@@ -43,7 +43,7 @@ int execute(char **command)
 		wait(NULL);
 		return (0);
 	}
-	if (is_kid == 0)
+/*	if (is_kid == 0)
 	{
 		if (execve(command[0], command, NULL) == -1)
 		{
@@ -51,7 +51,7 @@ int execute(char **command)
 			return (-1);
 		}
 	}
-
+*/
 	return (0);
 }
 
@@ -73,67 +73,4 @@ char **make_av(char *str)
 	}
 	toks[i] = NULL;
 	return (toks);
-}
-
-
-char clear(void)
-	{
-	return(0);
-/* move the cursor to the top left corner */
-/* clear the screen, and clear the scrollback buffer */
-/* clears the terminal text , return 0 on success, -1 for error. */
-	}
-
-
-char **p_strtok(char* a_str, char delim)
-		{
-			char** result = 0;
-			size_t count = 0;
-			char* tmp = a_str;
-			char* last_comma = 0;
-			char* token;
-
-			delim = ' ';
-/* Count how many elements will be extracted */
-			while (*tmp)
-			{
-				if (delim == *tmp)
-				{
-					count++;
-					last_comma = tmp;
-				}
-				tmp++;
-			}
-/* Add space for trailing token. */
-			count += last_comma < (a_str + strlen(a_str) - 1);
-			count++;
-/* Add space for terminating null string */
-
-			result = malloc(sizeof(char*) * count);
-			if (result)
-			{
-				size_t idx = 0;
-				token = strtok(a_str, &delim);
-				while (token)
-				{
-					assert(idx < count);
-					*(result +idx++) = strdup(token);
-					token = strtok(0, &delim);
-				}
-				assert(idx == count -1);
-				*(result + idx) = 0;
-			}
-			return (result);
-		}
-void print_array(char **array)
-{
-	int i = 0;
-
-	while (array[i] != NULL)
-	{
-		printf("%s\n", array[i]);
-		i++;
-	}
-	if (array[i] == NULL)
-		printf("NULL\n");
 }
